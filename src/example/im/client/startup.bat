a10e97b7e0b75497f11fa210a1a613862a93d06e
talent-tan <tywo45@163.com>
2017-05-30 19:23:15 +0800

    1.7.0.1

--- src/example/im/client/startup.bat
+++ src/example/im/client/startup.bat
@@ -5,5 +5,5 @@ setlocal & pushd
 set APP_ENTRY=org.tio.examples.im.client.ImClientStarter
 set BASE=%~dp0
 set CP=%BASE%\config;%BASE%\lib\*
-java -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-im-client-pid.hprof -cp "%CP%" %APP_ENTRY%
+java -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=4096 -XX:HeapDumpPath=c:/java-t-io-im-client-pid.hprof -cp "%CP%" %APP_ENTRY%
 endlocal & popd
--- src/example/im/server/startup.bat
+++ src/example/im/server/startup.bat
@@ -5,5 +5,5 @@ setlocal & pushd
 set APP_ENTRY=org.tio.examples.im.server.ImServerStarter
 set BASE=%~dp0
 set CP=%BASE%\config;%BASE%\lib\*
-java -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-im-server-pid.hprof -cp "%CP%" %APP_ENTRY%
+java -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=4096 -XX:HeapDumpPath=c:/java-t-io-im-server-pid.hprof -cp "%CP%" %APP_ENTRY%
 endlocal & popd
