6281f2e4428904512a880f7cc1d751173cb1287e
talent-tan <tywo45@163.com>
2019-04-01 15:27:45 +0800

    3.2.9发布：https://www.t-io.org/doc/tio/281

--- src/zoo/websocket/server/installAndCopy.bat
+++ src/zoo/websocket/server/installAndCopy.bat
@@ -1,5 +1,5 @@
 call mvn -Dmaven.test.skip=true clean install
 
 call rd ..\..\..\..\dist\examples\im\server /s /q
-call xcopy target\dist\tio-core-showcase-im-server-3.2.8.v20190326-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
+call xcopy target\dist\tio-core-showcase-im-server-3.2.9.v20190401-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
 
