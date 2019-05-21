c9e5564e2696280c6d72fb210de4c72eb26627eb
fanpan26 <fanpan26@126.com>
2019-05-21 19:53:21 +0800

    remove duplicate classes

--- src/zoo/websocket/starter/src/main/java/org/tio/websocket/starter/TioWebSocketIpStatListener.java
+++
@@ -1,9 +0,0 @@
-package org.tio.websocket.starter;
-
-import org.tio.core.stat.IpStatListener;
-
-/**
- * @author fanpan26
- * */
-public interface TioWebSocketIpStatListener extends IpStatListener {
-}
