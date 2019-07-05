764d95791f465eff582b3bcaa2e5d25396a7c213
talent-tan <tywo45@163.com>
2019-07-05 09:16:05 +0800

    update pom.xml and delete some not used files

--- src/zoo/websocket/client/installAndCopy.bat
+++
@@ -1,5 +0,0 @@
-call mvn -Dmaven.test.skip=true clean install
-
-rem call rd ..\..\..\..\dist\examples\im\server /s /q
-rem call xcopy target\dist\tio-core-showcase-im-server-3.3.3.v20190620-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
-
--- src/zoo/websocket/server/installAndCopy.bat
+++
@@ -1,5 +0,0 @@
-call mvn -Dmaven.test.skip=true clean install
-
-call rd ..\..\..\..\dist\examples\im\server /s /q
-call xcopy target\dist\tio-core-showcase-im-server-3.3.3.v20190620-RELEASE ..\..\..\..\dist\examples\im\server\ /s /e /q /y
-
