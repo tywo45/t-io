7e1e80a8f6f4d3ccb1cd1d0efb26b7d4f77aa614
fanpan26 <fanpan26@126.com>
2019-09-19 19:02:04 +0800

    rename tioWebSocketServerMarkBean to tioServerMarkBean

--- src/zoo/spring-boot/tio-core/src/main/java/org/tio/core/starter/TioServerMarkerConfiguration.java
+++ src/zoo/spring-boot/tio-core/src/main/java/org/tio/core/starter/TioServerMarkerConfiguration.java
@@ -206,7 +206,7 @@ import org.springframework.context.annotation.Configuration;
 public class TioServerMarkerConfiguration {
 
     @Bean
-    public Marker tioWebSocketServerMarkBean() {
+    public Marker tioServerMarkBean() {
         return new Marker();
     }
 
