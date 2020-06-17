5b1a2b74f5f53bfc8d0af2d3281c3100d4d9fc8b
talent-tan <tywo45@163.com>
2020-06-17 15:19:05 +0800

    允许自定义线程池的线程个数

--- src/utils/src/main/java/org/tio/utils/Threads.java
+++ src/utils/src/main/java/org/tio/utils/Threads.java
@@ -213,9 +213,9 @@ import org.tio.utils.thread.pool.TioCallerRunsPolicy;
 public class Threads {
 	public static final int					AVAILABLE_PROCESSORS	= Runtime.getRuntime().availableProcessors();
 	public static final int					CORE_POOL_SIZE			= AVAILABLE_PROCESSORS * 1;
-	public static final int					MAX_POOL_SIZE_FOR_TIO	= Math.max(CORE_POOL_SIZE * 3, 64);
-	public static final int					MAX_POOL_SIZE_FOR_GROUP	= Math.max(CORE_POOL_SIZE * 16, 256);
-	public static final long				KEEP_ALIVE_TIME			= 0L;											//360000L;
+	public static final int					MAX_POOL_SIZE_FOR_TIO	= Integer.getInteger("TIO_MAX_POOL_SIZE_FOR_TIO", Math.max(CORE_POOL_SIZE * 3, 64));
+	public static final int					MAX_POOL_SIZE_FOR_GROUP	= Integer.getInteger("TIO_MAX_POOL_SIZE_FOR_GROUP", Math.max(CORE_POOL_SIZE * 16, 256));
+	public static final long				KEEP_ALIVE_TIME			= 0L;																					//360000L;
 	@SuppressWarnings("unused")
 	private static final int				QUEUE_CAPACITY			= 1000000;
 	private static ThreadPoolExecutor		groupExecutor			= null;
